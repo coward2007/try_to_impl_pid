63201f09e75da298f9eb609aa63de018809500f8
coward2007 <289883421+coward2007@users.noreply.github.com>
2026-06-17 20:58:32 +0800

    Fix PID update calculation and variable names

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,5 +1,12 @@
 #include "pid.hpp"
 
 float PID::update(float setpoint, float measurement, float dt) {
+    float error = setpoint - measurement;
+    integral_ += error * dt;
+    float devivative = (error- prev_error_) / dt;
+    float output = config_.Kp  * error + config_.Ki * integral_ + config_.Kd * devivative;
+    prev_error_ =error;
+    return output; 
+
 
 }
